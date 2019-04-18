8f82e7f90973cf96ac2e99844797d4a1c00ecf38
James Carlson <jxxcarlson@mac.com>
2019-04-18 19:30:33 -0400

    Add: tests, set cell value, compute average value, compute next temperature value of cell

---
+++ src/HeatMap.elm
@@ -0,0 +1,135 @@
+module HeatMap exposing (HeatMap(..), location, index, cellAtIndex, setValue, averageAt, randomHeatMap)
+
+{-| This library is just a test.  I repeat: a test!
+
+@docs location, index
+
+-}
+
+import Array exposing(Array)
+import Random
+import List.Extra
+
+type HeatMap = HeatMap (Int, Int) (Array Float)
+
+rows : HeatMap  -> Int
+rows (HeatMap (rows_, _) _) = rows_
+
+cols : HeatMap              -> Int
+cols (HeatMap (r_, cols_) _) = cols_
+
+dimensions : HeatMap -> (Int, Int)
+dimensions (HeatMap idx _) =
+    idx
+
+location : Int -> (Int,  Int) -> Int
+location nRows (row, col) =
+    nRows * row + col
+
+index : (Int, Int) -> Int -> (Int, Int)
+index (nRows, nCols)  n =
+    (n // nCols, modBy nRows n)
+
+cellAtIndex: (Int,Int) -> HeatMap -> Float
+cellAtIndex (i,j) heatMap =
+    let
+        (HeatMap (nRows, _) array) = heatMap
+    in
+       Array.get (location nRows (i, j)) array
+         |> Maybe.withDefault 0
+
+
+setValue : HeatMap -> (Int,Int) -> Float -> HeatMap
+setValue (HeatMap (nRows, nCols) values) (i,j) value =
+    let
+       k = location nRows (i,j)
+    in
+       (HeatMap (nRows, nCols) (Array.set k value values))
+
+type CellType = Corner | Edge | Interior
+
+classifyCell : HeatMap -> (Int, Int) -> CellType
+classifyCell heatMap (i,j) =
+    let
+        (nRows, nCols) = dimensions heatMap
+        mri = nRows - 1
+        mci = nCols - 1
+     in
+     case i == 0 || j == 0 || i == mri || j == mci of
+         False -> Interior
+         True ->
+             if i == 0 && j == 0 then
+               Corner
+             else if i == 0 && j == mci then
+               Corner
+             else if i == mri && j == 0 then
+               Corner
+             else if i == mri && j == mci then
+               Corner
+             else
+               Edge
+
+
+
+
+
+
+averageAt : HeatMap -> (Int,Int) -> Float
+averageAt heatMap (i,j) =
+    let
+        east = cellAtIndex (i - 1,j) heatMap
+        west = cellAtIndex (i + 1,j) heatMap
+        north = cellAtIndex (i,j + 1) heatMap
+        south = cellAtIndex (i,j - 1 ) heatMap
+        denominator =
+            case classifyCell heatMap (i,j) of
+                Interior -> 4
+                Edge -> 3
+                Corner -> 2
+    in
+       (east + west + north + south)/denominator
+
+
+
+randomHeatMap : (Int, Int) -> HeatMap
+randomHeatMap (r,c) =
+       HeatMap (r,c) (Array.fromList <| floatSequence (r * c) 0 (0,1))
+
+
+nextCellValue : Float -> HeatMap -> (Int, Int) -> Float
+nextCellValue beta heatMap (i,j) =
+    let
+        currentCellValue = cellAtIndex (i,j) heatMap
+    in
+        (1 - beta) * currentCellValue + beta * (averageAt heatMap (i,j))
+
+---
+--- RNG
+---
+
+
+{-
+
+Example:
+
+> RNG.floatSequence 3 23 (0,1)
+[0.07049563320325747,0.8633668118636881,0.6762363032990798]
+
+-}
+
+floatSequence : Int -> Int -> (Float, Float) -> List Float
+floatSequence n k (a,b) =
+    floatSequence_ n (makeSeed k) (a,b)
+      |> Tuple.first
+
+gen : Int -> (Float, Float) -> Random.Generator (List Float)
+gen n  (a, b) =
+    Random.list n (Random.float a b)
+
+makeSeed : Int -> Random.Seed
+makeSeed k =
+    Random.initialSeed k
+
+floatSequence_ : Int -> Random.Seed -> (Float, Float) -> (List Float, Random.Seed)
+floatSequence_ n seed (a,b) =
+    Random.step (gen n (a,b)) seed
---
+++ tests/Example.elm
@@ -0,0 +1,42 @@
+module Example exposing (..)
+
+import Expect exposing (..)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+
+import HeatMap exposing(..)
+import Array exposing(Array)
+
+suite : Test
+suite =
+    let
+        cellList = List.range 0 10 |> List.map toFloat
+        heatMap = HeatMap (3,3) (Array.fromList cellList)
+        epsilon = 0.0000001
+
+    in
+    describe "The HeatMap module"
+            [ describe "Access functions" -- Nest as many descriptions as you like.
+                [ test "cellAtIndex"  <|
+                    \_ ->
+                        let
+                            c = HeatMap.cellAtIndex (2,2) heatMap
+                        in
+                            Expect.equal c 8.0
+
+                , test "average temperature at an interior cell" <|
+                    \_ ->
+                        averageAt heatMap (1,1)  |> Expect.within (Absolute epsilon) 4.0
+
+                , test "average temperature at an edge cell" <|
+                    \_ ->
+                        averageAt heatMap (0,1)  |> Expect.within (Absolute epsilon) 2.0
+
+                , test "average temperature at a corner cell" <|
+                    \_ ->
+                        averageAt heatMap (0,0)  |> Expect.within (Absolute epsilon) 2.0
+
+                , test "set cell value" <|
+                    \_ ->
+                        setValue heatMap (1,1) 0 |> cellAtIndex (1,1) |> Expect.within (Absolute epsilon) 0.0                ]
+            ]
