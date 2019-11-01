2e35ee89c3b77bcb4bdafc512c18736ddbebceac
Folkert <folkert@folkertdev.nl>
2019-11-01 11:16:31 +0100

    make HeatMap compile

--- examples/HeatMap/src/Image1.elm
+++ examples/HeatMap/src/Image1.elm
@@ -4,13 +4,12 @@ import Browser
 import Browser.Events exposing (onAnimationFrameDelta)
 import CellGrid exposing (CellGrid(..), Dimensions, Position, arrayIndex)
 import CellGrid.RenderWebGL exposing (CellStyle, Vertex, meshFromCellGrid)
-import Html exposing (Html)
-import Json.Decode exposing (Value)
-import Math.Vector3 as Vec3 exposing (Vec3, vec3)
-import WebGL exposing (Mesh)
 import Color exposing (Color)
+import Html exposing (Html)
+import WebGL exposing (Mesh)
 
-main : Program Value Float Float
+
+main : Program () Float Float
 main =
     Browser.element
         { init = \_ -> ( 0, Cmd.none )
@@ -22,44 +21,44 @@ main =
 
 view : Float -> Html msg
 view t =
-    CellGrid.RenderWebGL.meshToHtml { width = 400, height = 400 } mesh
-
-dimensions : Dimensions
-dimensions = Dimensions 10 10
-
-initializer : Int -> Int -> Int
-initializer i j = arrayIndex dimensions (Position i j)
+    CellGrid.RenderWebGL.meshToHtml { width = 1200, height = 1200 } mesh
 
 
-cg : CellGrid Int
-cg = CellGrid.initialize dimensions  (\i j -> i)
+cg : CellGrid Color
+cg =
+    let
+        dimensions : Dimensions
+        dimensions =
+            Dimensions 10 10
+
+        initializer : Int -> Int -> Color
+        initializer i j =
+            colorAtMatrixIndex dimensions (Position i j)
+    in
+    CellGrid.initialize dimensions initializer
 
 
-cellStyle : CellStyle Int
+cellStyle : CellStyle Color
 cellStyle =
-    { toColor =
-        \b ->
-            if modBy 2 b == 0 then
-                Color.black
-
-            else
-                Color.red
-    , cellWidth = 40
-    , cellHeight = 40
+    { toColor = identity
+    , cellWidth = 0.1
+    , cellHeight = 0.1
     }
 
-mesh = meshFromCellGrid cellStyle cg
+
+mesh : Mesh Vertex
+mesh =
+    meshFromCellGrid cellStyle cg
 
 
-
-colorAtMatrixIndex : ( Int, Int ) -> ( Int, Int ) -> Vec3
-colorAtMatrixIndex ( rows, cols ) ( i, j ) =
+colorAtMatrixIndex : Dimensions -> Position -> Color
+colorAtMatrixIndex dimensions position =
     let
         iRatio =
-            toFloat i / toFloat rows
+            toFloat position.row / toFloat dimensions.rows
 
         jRatio =
-            toFloat j / toFloat cols
+            toFloat position.column / toFloat dimensions.columns
 
         pi =
             3.1416
@@ -70,5 +69,4 @@ colorAtMatrixIndex ( rows, cols ) ( i, j ) =
         s2 =
             sin (4.1 * pi * jRatio)
     in
-    vec3 (0.5 + 0.3 * s1) 0.0 (0.5 + 0.5 * s2)
-
+    Color.rgb (0.5 + 0.3 * s1) 0.0 (0.5 + 0.5 * s2)
